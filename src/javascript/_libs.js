52d3f983a669fbcb6ee3888615c5963ee4ac5bac
Zhengyu Qian <laerhsif@gmail.com>
2021-02-08 21:39:34 +0800

    Add JS deps

---
+++ src/javascript/_libs.js
@@ -0,0 +1,10 @@
+/*!
+ * @license
+ * Lodash v4.17.15 | MIT license | https://lodash.com/
+ * axios v0.19.2 | Matt Zabriskie | MIT License | https://github.com/axios/axios
+ * dayjs.js | iamkun | MIT License | https://github.com/iamkun/dayjs
+ */
+
+// @codekit-append "_vendor/lodash.js";
+// @codekit-append "_vendor/axios.js";
+// @codekit-append "_vendor/dayjs.min.js";
