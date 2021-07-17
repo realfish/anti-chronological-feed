1ffd085294d9173533cfc55a43e10d1e1cb47b5c
Zhengyu Qian <laerhsif@gmail.com>
2021-07-17 11:36:00 +0800

    Switch to the local demo feed

--- src/javascript/view/index.js
+++ src/javascript/view/index.js
@@ -13,9 +13,9 @@ const DATE_CN_DAY = [
 	'十一', '十二', '十三', '十四', '十五', '十六', '十七' , '十八', '十九', '二十',
 	'二十一', '二十二', '二十三', '二十四', '二十五', '二十六', '二十七' , '二十八', '二十九', '三十', '三十一'
 ]
-// const API_FEED = 'assets/feed.xml';
+const API_FEED = 'assets/feed.xml';
 // const API_FEED = 'https://qingmang.me/users/131995/feed/';
-const API_FEED = 'https://realfish-cors-anywhere.herokuapp.com/https://qingmang.me/users/131995/feed/';
+// const API_FEED = 'https://realfish-cors-anywhere.herokuapp.com/https://qingmang.me/users/131995/feed/';
 axios.get(API_FEED)
 .then(res => res.data)
 .then(data => new win.DOMParser().parseFromString(data, "text/xml"))
